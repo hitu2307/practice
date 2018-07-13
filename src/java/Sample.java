d788d7e0c377c6dff5c1f1010e1327e021f893f3
RAWAL Hitesh <HRL1@dsone.3ds.com>
2018-07-13 16:32:02 +0530

    first push

---
+++ src/java/Sample.java
@@ -0,0 +1,11 @@
+package com.ds.learning;
+
+public class Sample {
+	public static void main(String[] ARGV) {
+		System.out.println("Hello 3DS");
+		String commandSymbolicName = "command_APPRouteAddPeople on program eServiceSchemaVariableMapping.tcl to command APPRouteAddPeople";
+		commandSymbolicName = commandSymbolicName.substring(0, commandSymbolicName.indexOf("on program eServiceSchemaVariableMapping")-1);
+		System.out.println("commandSymbolicName : " + commandSymbolicName);
+	}
+
+}
